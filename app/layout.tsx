49e55c95664987fc3d2177ad14f7602e4ffa2801
Grenish rai <107925840+Grenish@users.noreply.github.com>
2025-06-08 16:55:10 +0530

    Add RootLayout component with Montserrat font and metadata

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+
+const mont = Montserrat({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  style: "normal",
+  display: "swap"
+})
+
+export const metadata: Metadata = {
+  title: "T3 Chat Clone",
+  description: "T3 Chat is a clone app of the popular chat web application T3 Chat. But this clone is better.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${mont.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
